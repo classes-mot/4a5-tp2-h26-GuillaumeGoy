049d6f65038b3d024b59c2efeb29afc6a533e878
Guillaume <guillaumegface@gmail.com>
2026-03-20 10:45:22 -0400

    Creation: Creation de app.js basic pour le backend.

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import userRoutes from "./routes/users-routes.js";
+import jeuxRoutes from "./routes/jeux-routes.jks";
+import errorHandler from "./hander/error-handler.js";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json);
+
+app.use("/api/jeux", jeuxRoutes);
+
+app.use("api/users", userRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Route non trouve");
+  error.code = 404;
+  next(error);
+});
+
+app.use(errorHandler);
+
+app.listen(5000, () => {
+  console.log("serveur ecoute au", "http://localhost:5000");
+});
