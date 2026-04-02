8c590d155b2c13a9a9477fc5752eba8d116222a0
Guillaume <guillaumegface@gmail.com>
2026-04-02 09:21:11 -0400

    Changement du app pour enlever cors

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,14 @@
 import express from "express";
 import cors from "cors";
 import userRoutes from "./routes/users-routes.js";
-import jeuxRoutes from "./routes/jeux-routes.jks";
+import jeuxRoutes from "./routes/jeux-routes.js";
 import errorHandler from "./hander/error-handler.js";
 
 const app = express();
 
 app.use(cors());
 
-app.use(express.json);
+app.use(express.json());
 
 app.use("/api/jeux", jeuxRoutes);
 
