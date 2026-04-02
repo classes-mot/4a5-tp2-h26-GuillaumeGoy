17077ed3cee21a3b644edb515324bb7d2fcdad03
Guillaume <guillaumegface@gmail.com>
2026-04-02 09:24:33 -0400

    Creation et codage de user-routes.js

---
+++ backend/routes/users-routes.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+import usersController from "../controllers/users-controller.js";
+const router = express.Router();
+
+router.get("/", usersController.getUsers);
+
+router.get("/profile/:uid", usersController.getUserById);
+
+router.post("/register", usersController.registerUser);
+
+router.post("/login", usersController.login);
+
+export default router;
