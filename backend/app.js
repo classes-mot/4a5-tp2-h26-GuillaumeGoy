125f4b1edbcf56579584db3105f651c353ae6bc8
Guillaume <guillaumegface@gmail.com>
2026-04-03 18:51:30 -0400

    CODAGE/DEBUG: Fix de app.js pour enlever CORS complete et rajouter DB + header

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,29 @@
 import express from "express";
-import cors from "cors";
 import userRoutes from "./routes/users-routes.js";
 import jeuxRoutes from "./routes/jeux-routes.js";
+
 import errorHandler from "./handler/error-handler.js";
+import { connectDB } from "./utils/bd.js";
+
+await connectDB;
 
 const app = express();
 
-app.use(cors());
-
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-AllowHeaders",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+  next();
+});
 
 app.use("/api/jeux", jeuxRoutes);
 
-app.use("api/users", userRoutes);
+app.use("/api/users", userRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Route non trouve");
